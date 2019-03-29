94fe01c05f71c1f1d388d69e4b5ef659f98c6e99
jubayer <khondokereftakharjubayer@gmail.com>
2019-03-30 01:51:32 +0600

    math operator

---
+++ math.js
@@ -0,0 +1,35 @@
+//cost of eggs calculate
+
+var eggPrice = 5 ;
+var numberOfEggs = 12 ;
+var totalCostOfEggs = eggPrice * numberOfEggs;
+console.log("Total cost of eggs is" , totalCostOfEggs,"taka .");
+
+//area of circle calculate = pi * r square
+
+var radius = 7;
+const pi = 3.14159;
+var areaOfCircle = pi * (radius * radius);
+console.log("Area of the circle is",areaOfCircle,"sq inch");
+
+//bdt to usd calculator
+
+var bdtToUsd = 83.5;
+var bdt = 5000;
+var usd = bdt / bdtToUsd;
+console.log("$",usd);
+
+//remainder
+
+var r1 = 5;
+var r2 = 2;
+var remainder = r1 % r2;
+console.log("The remainder is",remainder);
+
+
+
+
+
+
+
+
---
+++ variable-01.js
@@ -0,0 +1,3 @@
+var myName = "Jubayer";
+var numder = 1234;
+console.log(myName, numder);
